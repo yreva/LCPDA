bdd5b4119642aaf4202dd1e6baaeca4d3f19fed9
Avery (mb) <abd@daltonianscientific.com>
2025-03-17 22:01:20 -0700

    fix bug for resolution messing up clicks on chromatogram

--- RVPDA/Models/PlotModel.cs
+++ RVPDA/Models/PlotModel.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Media;
 using System.Xml.Linq;
 using Microsoft.VisualBasic;
 using OpenTK.Windowing.GraphicsLibraryFramework;
@@ -885,7 +886,22 @@ namespace RVPDA.Models
                 var mouse = e.GetPosition(_chromatogramPlot);
                 var x = mouse.X;
                 var y = mouse.Y;
+
+                // Get the DPI scale factor
+                PresentationSource source = PresentationSource.FromVisual((Visual)sender);
+                double dpiX = 1.0, dpiY = 1.0;
+                if (source?.CompositionTarget != null)
+                {
+                    dpiX = source.CompositionTarget.TransformToDevice.M11;
+                    dpiY = source.CompositionTarget.TransformToDevice.M22;
+                }
+
+                // Adjust coordinates
+                x *= dpiX;
+                y *= dpiY;
+
                 double clickedX = _chromatogramPlot.Plot.GetCoordinates(new Pixel(x, y)).X;
+
 
                 // Find the closest time point
                 double nearestTime = _chromatogramViewModel.Times.OrderBy(x => Math.Abs(x - clickedX)).FirstOrDefault();
